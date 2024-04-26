06109720be6b2c69ed03992b0f5a5c62f37fc8db
Martin Kohen Soria <martinkohen6@gmail.com>
2024-04-26 16:44:52 -0300

    agregamos fotos y terminamos la pagina principal

--- index.php
+++ index.php
@@ -41,9 +41,7 @@
 
                             <div class="collapse navbar-collapse main-menu-item" id="navbarSupportedContent">
                                 <ul class="navbar-nav">
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="index.php">Home</a>
-                                    </li>
+
                                     <li class="nav-item">
                                         <a class="nav-link" href="contact.php">Sobre nosotros</a>
                                     </li>
@@ -74,10 +72,7 @@
                         <div class="banner_text">
                             <div class="banner_text_iner">
                                 <h1>Dream Desert</h1>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                    sed do eiusmod tempor incididunt ut labore et dolore
-                                    magna aliqua. Quis ipsum </p>
-                                <a href="#" class="btn_1">Watch Tutorial</a>
+                                <p>Este video juego apasionante basado en el aspecto de la famosa serie de Netflix titulada "Breaking Bad" nos contará la historia de Walter Black, un frustrado profesor de química de la Universidad de Nueva México. </p>
                             </div>
                         </div>
                     </div>
@@ -86,84 +81,29 @@
         </section>
         <!-- banner part start-->
 
-    
-                    
+
+
         <!--::about_us part start::-->
         <section class="live_stareams padding_bottom">
             <div class="container-fluid">
                 <div class="row align-items-center justify-content-between">
                     <div class="col-md-2 offset-lg-2 offset-sm-1">
                         <div class="live_stareams_text">
-                            <h2>  Imagenes</h2>
-                            <div class="btn_1">Descargar</div>
+                            <h2> Imagenes</h2>
                         </div>
                     </div>
                     <div class="col-md-7 offset-sm-1">
                         <div class="live_stareams_slide owl-carousel">
                             <div class="live_stareams_slide_img">
-                                <img src="img/live_streams_1.png" alt="">
-                                <div class="extends_video">
-                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
-                                        <span class="fas fa-play"></span>
-                                    </a>
-                                </div>
+                                <img src="img/Black.png" alt="">
                             </div>
                             <div class="live_stareams_slide_img">
-                                <img src="img/live_streams_2.png" alt="">
-                                <div class="extends_video">
-                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
-                                        <span class="fas fa-play"></span>
-                                    </a>
-                                </div>
+                                <img src="img/capilla.png" alt="">
                             </div>
                             <div class="live_stareams_slide_img">
-                                <img src="img/live_streams_1.png" alt="">
-                                <div class="extends_video">
-                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
-                                        <span class="fas fa-play"></span>
-                                    </a>
-                                </div>
+                                <img src="img/carpa.png" alt="">
                             </div>
-                            <div class="live_stareams_slide_img">
-                                <img src="img/live_streams_2.png" alt="">
-                                <div class="extends_video">
-                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
-                                        <span class="fas fa-play"></span>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="live_stareams_slide_img">
-                                <img src="img/live_streams_1.png" alt="">
-                                <div class="extends_video">
-                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
-                                        <span class="fas fa-play"></span>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="live_stareams_slide_img">
-                                <img src="img/live_streams_2.png" alt="">
-                                <div class="extends_video">
-                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
-                                        <span class="fas fa-play"></span>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="live_stareams_slide_img">
-                                <img src="img/live_streams_1.png" alt="">
-                                <div class="extends_video">
-                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
-                                        <span class="fas fa-play"></span>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="live_stareams_slide_img">
-                                <img src="img/live_streams_2.png" alt="">
-                                <div class="extends_video">
-                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
-                                        <span class="fas fa-play"></span>
-                                    </a>
-                                </div>
-                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -171,7 +111,7 @@
         </section>
         <!--::about_us part end::-->
 
-        
+
 
         <!-- gallery_part part start-->
         <section class="gallery_part section_padding">
@@ -188,7 +128,8 @@
                         <div class="gallery_part_item">
                             <div class="grid">
                                 <div class="grid-sizer"></div>
-                                <a href="img/gallery/gallery_item_1.png" class="grid-item bg_img img-gal grid-item--height-1" style="background-image: url('img/gallery/gallery_item_1.png')">
+                                
+                                <a href="img/enemigo1.png" class="grid-item bg_img img-gal" style="background-image: url('img/enemigo1.png')">
                                     <div class="single_gallery_item">
                                         <div class="single_gallery_item_iner">
                                             <div class="gallery_item_text">
@@ -197,7 +138,7 @@
                                         </div>
                                     </div>
                                 </a>
-                                <a href="img/gallery/gallery_item_2.png" class="grid-item bg_img img-gal" style="background-image: url('img/gallery/gallery_item_2.png')">
+                                <a href="img/enemigo2.png" class="grid-item bg_img img-gal" style="background-image: url('img/enemigo2.png')">
                                     <div class="single_gallery_item">
                                         <div class="single_gallery_item_iner">
                                             <div class="gallery_item_text">
@@ -206,7 +147,7 @@
                                         </div>
                                     </div>
                                 </a>
-                                <a href="img/gallery/gallery_item_3.png" class="grid-item bg_img img-gal" style="background-image: url('img/gallery/gallery_item_3.png')">
+                                <a href="img/enemigo3.png" class="grid-item bg_img img-gal" style="background-image: url('img/enemigo3.png')">
                                     <div class="single_gallery_item">
                                         <div class="single_gallery_item_iner">
                                             <div class="gallery_item_text">
@@ -215,42 +156,10 @@
                                         </div>
                                     </div>
                                 </a>
-                                <a href="img/gallery/gallery_item_5.png" class="grid-item bg_img img-gal grid-item--height-2" style="background-image: url('img/gallery/gallery_item_5.png')">
-                                    <div class="single_gallery_item">
-                                        <div class="single_gallery_item_iner">
-                                            <div class="gallery_item_text">
-                                                <i class="ti-zoom-in"></i>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a href="img/gallery/gallery_item_7.png" class="grid-item bg_img img-gal grid-item--height-2" style="background-image: url('img/gallery/gallery_item_7.png')">
-                                    <div class="single_gallery_item">
-                                        <div class="single_gallery_item_iner">
-                                            <div class="gallery_item_text">
-                                                <i class="ti-zoom-in"></i>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a href="img/gallery/gallery_item_6.png" class="grid-item bg_img img-gal grid-item--width-1" style="background-image: url('img/gallery/gallery_item_6.png')">
-                                    <div class="single_gallery_item">
-                                        <div class="single_gallery_item_iner">
-                                            <div class="gallery_item_text">
-                                                <i class="ti-zoom-in"></i>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a href="img/gallery/gallery_item_4.png" class="grid-item bg_img img-gal sm_weight  grid-item--height-1" style="background-image: url('img/gallery/gallery_item_4.png')">
-                                    <div class="single_gallery_item">
-                                        <div class="single_gallery_item_iner">
-                                            <div class="gallery_item_text">
-                                                <i class="ti-zoom-in"></i>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
+                                
+                            
+                            
+                                
                             </div>
                         </div>
                     </div>
@@ -261,7 +170,7 @@
 
 
         <!-- pricing part start-->
-    
+
 
         <!--::footer_part start::-->
         <footer class="footer_part">
@@ -274,13 +183,13 @@
                                     Copyright &copy;
                                     <script>
                                         document.write(new Date().getFullYear());
-                                    </script> 
+                                    </script>
                                     Pagina diseñada por los estudiantes de 5°10
-                                    
+
                                 </P>
                             </div>
                         </div>
-                    
+
                     </div>
                 </div>
             </div>
