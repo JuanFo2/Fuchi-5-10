31d9ba59ec4013734465cd373f40967eee4c0f32
Juan Ojeda <juanfrancoob@gmail.com>
2024-04-26 15:29:58 -0300

    primera version

---
+++ index.php
@@ -0,0 +1,320 @@
+<!doctype html>
+<html lang="zxx">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Desert Dream</title>
+    <link rel="icon" href="img/Logofuchi.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="css/all.css">
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="css/slick.css">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="body_bg">
+        <!--::header part start::-->
+        <header class="main_menu single_page_menu">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-12">
+                        <nav class="navbar navbar-expand-lg navbar-light">
+                            <a class="navbar-brand" href="index.php"> <img src="img/LogoJuego.png" width="200px" height="150px" alt="logo"> </a>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="menu_icon"><i class="fas fa-bars"></i></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse main-menu-item" id="navbarSupportedContent">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="index.php">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="contact.php">Sobre nosotros</a>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Cuenta
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="blog.html"> Iniciar Sesión</a>
+                                            <a class="dropdown-item" href="single-blog.html">Registrarse</a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <a href="#" class="btn_1 d-none d-sm-block">Instalar ahora</a>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- Header part end-->
+
+        <!-- banner part start-->
+        <section class="banner_part">
+            <div class="container">
+                <div class="row align-items-center justify-content-between">
+                    <div class="col-lg-6 col-md-8">
+                        <div class="banner_text">
+                            <div class="banner_text_iner">
+                                <h1>Dream Desert</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore
+                                    magna aliqua. Quis ipsum </p>
+                                <a href="#" class="btn_1">Watch Tutorial</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- banner part start-->
+
+    
+                    
+        <!--::about_us part start::-->
+        <section class="live_stareams padding_bottom">
+            <div class="container-fluid">
+                <div class="row align-items-center justify-content-between">
+                    <div class="col-md-2 offset-lg-2 offset-sm-1">
+                        <div class="live_stareams_text">
+                            <h2>  Imagenes</h2>
+                            <div class="btn_1">Descargar</div>
+                        </div>
+                    </div>
+                    <div class="col-md-7 offset-sm-1">
+                        <div class="live_stareams_slide owl-carousel">
+                            <div class="live_stareams_slide_img">
+                                <img src="img/live_streams_1.png" alt="">
+                                <div class="extends_video">
+                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
+                                        <span class="fas fa-play"></span>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="live_stareams_slide_img">
+                                <img src="img/live_streams_2.png" alt="">
+                                <div class="extends_video">
+                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
+                                        <span class="fas fa-play"></span>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="live_stareams_slide_img">
+                                <img src="img/live_streams_1.png" alt="">
+                                <div class="extends_video">
+                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
+                                        <span class="fas fa-play"></span>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="live_stareams_slide_img">
+                                <img src="img/live_streams_2.png" alt="">
+                                <div class="extends_video">
+                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
+                                        <span class="fas fa-play"></span>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="live_stareams_slide_img">
+                                <img src="img/live_streams_1.png" alt="">
+                                <div class="extends_video">
+                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
+                                        <span class="fas fa-play"></span>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="live_stareams_slide_img">
+                                <img src="img/live_streams_2.png" alt="">
+                                <div class="extends_video">
+                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
+                                        <span class="fas fa-play"></span>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="live_stareams_slide_img">
+                                <img src="img/live_streams_1.png" alt="">
+                                <div class="extends_video">
+                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
+                                        <span class="fas fa-play"></span>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="live_stareams_slide_img">
+                                <img src="img/live_streams_2.png" alt="">
+                                <div class="extends_video">
+                                    <a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
+                                        <span class="fas fa-play"></span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--::about_us part end::-->
+
+        
+
+        <!-- gallery_part part start-->
+        <section class="gallery_part section_padding">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-5">
+                        <div class="section_tittle text-center">
+                            <h2>Enemigos</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="gallery_part_item">
+                            <div class="grid">
+                                <div class="grid-sizer"></div>
+                                <a href="img/gallery/gallery_item_1.png" class="grid-item bg_img img-gal grid-item--height-1" style="background-image: url('img/gallery/gallery_item_1.png')">
+                                    <div class="single_gallery_item">
+                                        <div class="single_gallery_item_iner">
+                                            <div class="gallery_item_text">
+                                                <i class="ti-zoom-in"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                                <a href="img/gallery/gallery_item_2.png" class="grid-item bg_img img-gal" style="background-image: url('img/gallery/gallery_item_2.png')">
+                                    <div class="single_gallery_item">
+                                        <div class="single_gallery_item_iner">
+                                            <div class="gallery_item_text">
+                                                <i class="ti-zoom-in"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                                <a href="img/gallery/gallery_item_3.png" class="grid-item bg_img img-gal" style="background-image: url('img/gallery/gallery_item_3.png')">
+                                    <div class="single_gallery_item">
+                                        <div class="single_gallery_item_iner">
+                                            <div class="gallery_item_text">
+                                                <i class="ti-zoom-in"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                                <a href="img/gallery/gallery_item_5.png" class="grid-item bg_img img-gal grid-item--height-2" style="background-image: url('img/gallery/gallery_item_5.png')">
+                                    <div class="single_gallery_item">
+                                        <div class="single_gallery_item_iner">
+                                            <div class="gallery_item_text">
+                                                <i class="ti-zoom-in"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                                <a href="img/gallery/gallery_item_7.png" class="grid-item bg_img img-gal grid-item--height-2" style="background-image: url('img/gallery/gallery_item_7.png')">
+                                    <div class="single_gallery_item">
+                                        <div class="single_gallery_item_iner">
+                                            <div class="gallery_item_text">
+                                                <i class="ti-zoom-in"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                                <a href="img/gallery/gallery_item_6.png" class="grid-item bg_img img-gal grid-item--width-1" style="background-image: url('img/gallery/gallery_item_6.png')">
+                                    <div class="single_gallery_item">
+                                        <div class="single_gallery_item_iner">
+                                            <div class="gallery_item_text">
+                                                <i class="ti-zoom-in"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                                <a href="img/gallery/gallery_item_4.png" class="grid-item bg_img img-gal sm_weight  grid-item--height-1" style="background-image: url('img/gallery/gallery_item_4.png')">
+                                    <div class="single_gallery_item">
+                                        <div class="single_gallery_item_iner">
+                                            <div class="gallery_item_text">
+                                                <i class="ti-zoom-in"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- gallery_part part end-->
+
+
+        <!-- pricing part start-->
+    
+
+        <!--::footer_part start::-->
+        <footer class="footer_part">
+            <div class="copygight_text">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-8">
+                            <div class="copyright_text">
+                                <P><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                    Copyright &copy;
+                                    <script>
+                                        document.write(new Date().getFullYear());
+                                    </script> 
+                                    Pagina diseñada por los estudiantes de 5°10
+                                    
+                                </P>
+                            </div>
+                        </div>
+                    
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!--::footer_part end::-->
+    </div>
+
+
+    <!-- jquery plugins here-->
+    <script src="js/jquery-1.12.1.min.js"></script>
+    <!-- popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- easing js -->
+    <script src="js/jquery.magnific-popup.js"></script>
+    <!-- swiper js -->
+    <script src="js/swiper.min.js"></script>
+    <!-- swiper js -->
+    <script src="js/masonry.pkgd.js"></script>
+    <!-- particles js -->
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <!-- slick js -->
+    <script src="js/slick.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/contact.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.form.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/mail-script.js"></script>
+    <!-- custom js -->
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
