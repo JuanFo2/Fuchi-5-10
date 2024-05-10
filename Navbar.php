aaa5cd7d083f3528ebcfc33f56da22ca91554cf9
Shandee Incapuiño <shandeequispe13@gmail.com>
2024-05-10 16:48:41 -0300

    borradas fotos

---
+++ Navbar.php
@@ -0,0 +1,36 @@
+<header class="main_menu single_page_menu">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-12">
+                        <nav class="navbar navbar-expand-lg navbar-light">
+                            <a class="navbar-brand" href="index.php"> <img src="img/LogoJuego.png" width="200px" height="150px" alt="logo"> </a>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="menu_icon"><i class="fas fa-bars"></i></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse main-menu-item" id="navbarSupportedContent">
+                                <ul class="navbar-nav">
+                                <li class="nav-item">
+                                        <a class="nav-link" href="index.php">Home</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="contact.php">Sobre nosotros</a>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Cuenta
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="blog.html"> Iniciar Sesión</a>
+                                            <a class="dropdown-item" href="single-blog.html">Registrarse</a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <a href="#" class="btn_1 d-none d-sm-block">Instalar ahora</a>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
